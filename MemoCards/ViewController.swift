cfe8c05516a590b3e8dae1fbbe0dece0af2b4c86
danylonovosad <danylo.novosad@gmail.com>
2018-01-05 13:55:45 +0200

    Initial Commit

---
+++ MemoCards/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MemoCards
+//
+//  Created by Ostin Ostwald on 1/5/18.
+//  Copyright © 2018 Ostin Ostwald. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
